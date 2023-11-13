7fc03b7c6c7064fd3087e798317dc0d2185c78f1
kardachh <kardakov.denis49@gmail.com>
2023-11-13 15:58:40 +0300

    added garmonichnaya-lichnost added mobile menu

--- vyatsu2030/js/main.js
+++ vyatsu2030/js/main.js
@@ -17,13 +17,40 @@ const addEvents = () => {
         });
     })
 
+    tippy('#nav-mobile', {
+        theme: 'custom-white', allowHTML: true, interactive: true, hideOnClick: false, // offset:[0,40],
+        content: `
+                <div class="bg-white text-black normal-case text-center">
+                    <ul class="space-y-5 uppercase text-[13px] font-bold tracking-[1.92px]">
+                        <li>
+                            <a href="/vyatsu-web/vyatsu2030/#" class="hover:text-red-600">
+                                О программе
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/vyatsu-web/vyatsu2030/#projects" class="hover:text-red-600">
+                                Проекты и политики
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/vyatsu-web/vyatsu2030/#news" class="hover:text-red-600">
+                                Новости
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/vyatsu-web/vyatsu2030/#directorate" class="hover:text-red-600">
+                                Дирекция
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            `
+    })
+
     // tippy for "Стратегические проекты" links menu
     // nav__link-with-popup
-    tippy('.nav__link-with-popup',{
-        theme: 'custom-white',
-        allowHTML: true,
-        interactive: true,
-        // offset:[0,40],
+    tippy('.nav__link-with-popup', {
+        theme: 'custom-white', allowHTML: true, interactive: true, hideOnClick: false, // offset:[0,40],
         content: `
                 <div class="bg-white text-black normal-case text-center">
                     <ul class="space-y-5">
@@ -34,16 +61,13 @@ const addEvents = () => {
                             <a href="/vyatsu-web/vyatsu2030/projects/sreda-obitania/">Среда<br>обитания</a>
                         </li>
                         <li>
-                            <a href="../projects#">Гармоничная<br>личность</a>
+                            <a href="/vyatsu-web/vyatsu2030/projects/garmonichnaya-lichnost/">Гармоничная<br>личность</a>
                         </li>
                     </ul>
                 </div>
             `
     })
-    console.log(window.location.pathname)
-    console.log(window.location.pathname.split('/'))
 }
-
 
 
 window.onload = addEvents
